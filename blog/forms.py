b1af89eb990d36a6d1f615dae2618d32b2d26f53
Patt Höge <paty.hoge98@gmail.com>
2023-10-26 19:11:15 +0000

    Create comment post to the comment section

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['body'].label = ''  # Set an empty label for the body field
+        self.fields['body'].widget = forms.Textarea(
+            attrs={'placeholder': 'Leave a comment...'})
