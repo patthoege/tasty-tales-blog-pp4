7ad26be24287b190d6a438abc2fd0b9afeae7a91
Patt Höge <paty.hoge98@gmail.com>
2023-11-26 20:35:12 +0000

    Display and format Django DurationField in template

---
+++ blog/templatetags/custom_filters.py
@@ -0,0 +1,17 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.dateparse import parse_duration
+
+
+# Formatting and Displaying Django's DurationField in Templates
+# https://docs.djangoproject.com/en/3.2/howto/custom-template-tags/#writing-custom-template-filters
+# https://copyprogramming.com/howto/display-and-format-django-durationfield-in-template#durationfield-format
+register = template.Library()
+
+@register.filter(name='duration')
+@stringfilter
+def duration_format(value):
+    duration = parse_duration(value)
+    total_seconds = duration.total_seconds()
+    minutes, seconds = divmod(total_seconds, 60)
+    return f"{int(minutes):02d}:{int(seconds):02d}"
