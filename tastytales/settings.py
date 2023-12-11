b82400b69a75cad54028f3fcd7046c8f14aa7aa9
Patt Höge <paty.hoge98@gmail.com>
2023-12-11 09:53:09 +0000

    Test deployment

--- tastytales/settings.py
+++ tastytales/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['tasty-tales-pp4.herokuapp.com', '8000-patthoege-tastytalesblo-iiznuaza0dh.ws-eu106.gitpod.io']
 
 
