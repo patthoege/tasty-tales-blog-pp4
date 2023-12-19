1ff388bf2bfc74d409ba78ea2957a5c699a60765
Patt Höge <paty.hoge98@gmail.com>
2023-12-19 11:34:56 +0000

    Clean style sheet and add media query to the hero content for small devices

--- tastytales/settings.py
+++ tastytales/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['tasty-tales-pp4.herokuapp.com', '8000-patthoege-tastytalesblo-31iz79bxz17.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['tasty-tales-pp4.herokuapp.com', '8000-patthoege-tastytalesblo-31iz79bxz17.ws-eu107.gitpod.io']
 
 
 # Application definition
@@ -86,10 +86,10 @@ MIDDLEWARE = [
 ]
 
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ['https://tasty-tales-pp4.herokuapp.com', 'https://8000-patthoege-tastytalesblo-31iz79bxz17.ws-eu106.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://tasty-tales-pp4.herokuapp.com', 'https://8000-patthoege-tastytalesblo-31iz79bxz17.ws-eu107.gitpod.io']
 CORS_ALLOWED_ORIGINS = [
     "https://tasty-tales-pp4.herokuapp.com",
-    "https://8000-patthoege-tastytalesblo-31iz79bxz17.ws-eu106.gitpod.io",
+    "https://8000-patthoege-tastytalesblo-31iz79bxz17.ws-eu107.gitpod.io",
 ]
 
 ROOT_URLCONF = 'tastytales.urls'
